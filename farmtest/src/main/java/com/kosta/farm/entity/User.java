a320504ab631513a6215c178ef1ae5eba7aba4f8
Jisu <139774652+serendipity63@users.noreply.github.com>
2023-11-29 11:07:02 +0900

    test

---
+++ farmtest/src/main/java/com/kosta/farm/entity/User.java
@@ -0,0 +1,36 @@
+package com.kosta.farm.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Builder
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String password;
+	private String address1;
+	private String address2;
+	private String address3;
+
+	
+}

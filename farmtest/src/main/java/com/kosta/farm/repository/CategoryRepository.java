261c058e7831b6a34f625db6748c39f08bec239c
Jisu <139774652+serendipity63@users.noreply.github.com>
2023-11-30 14:50:07 +0900

    pull from dev

---
+++ farmtest/src/main/java/com/kosta/farm/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.kosta.farm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kosta.farm.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
---
+++ farmtest/src/main/java/com/kosta/farm/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.kosta.farm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kosta.farm.entity.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+}

d8fa4730b6aebf2bcde3073e41f404037253ee8f
JeHa-An <anjeha048@gmail.com>
2023-12-01 18:13:16 +0900

    231201 1813

--- farmtest/src/main/java/com/kosta/farm/repository/FarmerDslRepository.java
+++ farmtest/src/main/java/com/kosta/farm/repository/FarmerDslRepository.java
@@ -22,34 +22,34 @@ public class FarmerDslRepository {
 	@Autowired
 	private JPAQueryFactory jpaQueryFactory;
 	
-	// 파머페이지 견적현황 state : 0 (대기중), 1(기간만료), 2(결제완료) , 페이지 정보
-	public List<Tuple> findQuotationByFarmerIdAndStateAndPaging(Long farmerId, String state, PageRequest pageRequest) {
-		QQuotation quotation = QQuotation.quotation;
-		QRequest request = QRequest.request;
-		return jpaQueryFactory.select(quotation, request.address).distinct()
-				.from(quotation)
-				.join(request)
-				.on(quotation.requestId.eq(request.requestId))
-				.where(quotation.farmerId.eq(farmerId)
-						.and(quotation.state.eq(state))
-						.and(request.requestState.eq("1"))
-						)
-				.orderBy(quotation.quotationId.desc())
-				.offset(pageRequest.getOffset())
-				.limit(pageRequest.getPageSize())
-				.fetch();
-	}
-	
-	// 
-	public Long findQuotationCountByFarmerId(Long farmerId, String state) {
-		QQuotation quotation = QQuotation.quotation;
-		return jpaQueryFactory.select(quotation.count())
-				.from(quotation)
-				.where(quotation.farmerId.eq(farmerId)
-						.and(quotation.state.eq(state)))
-				.fetchOne();
-	}
-	
+//	// 파머페이지 견적현황 state : 0 (대기중), 1(기간만료), 2(결제완료) , 페이지 정보
+//	public List<Tuple> findQuotationByFarmerIdAndStateAndPaging(Long farmerId, String state, PageRequest pageRequest) {
+//		QQuotation quotation = QQuotation.quotation;
+//		QRequest request = QRequest.request;
+//		return jpaQueryFactory.select(quotation, request.address).distinct()
+//				.from(quotation)
+//				.join(request)
+//				.on(quotation.requestId.eq(request.requestId))
+//				.where(quotation.farmerId.eq(farmerId)
+//						.and(quotation.state.eq(state))
+//						.and(request.requestState.eq("1"))
+//						)
+//				.orderBy(quotation.quotationId.desc())
+//				.offset(pageRequest.getOffset())
+//				.limit(pageRequest.getPageSize())
+//				.fetch();
+//	}
+//	
+//	// 
+//	public Long findQuotationCountByFarmerId(Long farmerId, String state) {
+//		QQuotation quotation = QQuotation.quotation;
+//		return jpaQueryFactory.select(quotation.count())
+//				.from(quotation)
+//				.where(quotation.farmerId.eq(farmerId)
+//						.and(quotation.state.eq(state)))
+//				.fetchOne();
+//	}
+//	
 //	public List<Tuple> findOrdersByFarmerIdAndOrdersState(Long farmerId, String state, PageInfo pageInfo) {
 //		QOrders orders = QOrders.orders;
 //		QRequest request = QRequest.request;

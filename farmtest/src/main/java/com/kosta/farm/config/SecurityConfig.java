75f1e2b582ead71fe5c64837da60efc4710651c6
heechan <heechan0023@naver.com>
2023-12-22 09:52:56 +0900

    231222 push before merge

--- farmtest/src/main/java/com/kosta/farm/config/SecurityConfig.java
+++ farmtest/src/main/java/com/kosta/farm/config/SecurityConfig.java
@@ -14,8 +14,6 @@ import org.springframework.web.filter.CorsFilter;
 import com.kosta.farm.config.jwt.JwtTokenFilter;
 import com.kosta.farm.config.oauth2.OAuth2LoginSuccessHandler;
 import com.kosta.farm.config.oauth2.PrincipalOAuth2UserService;
-//import com.kosta.farm.config.oauth2.OAuth2LoginSuccessHandler;
-//import com.kosta.farm.config.oauth2.PrincipalOauth2UserService;
 import com.kosta.farm.service.UserService;
 
 import lombok.RequiredArgsConstructor;

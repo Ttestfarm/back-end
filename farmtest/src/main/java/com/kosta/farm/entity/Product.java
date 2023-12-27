037f7aa17aadf68e4d691faea649108b46487217
JeHa-An <anjeha048@gmail.com>
2023-12-27 09:42:59 +0900

    farmerService 수정

--- farmtest/src/main/java/com/kosta/farm/entity/Product.java
+++ farmtest/src/main/java/com/kosta/farm/entity/Product.java
@@ -46,8 +46,8 @@ public class Product {
 	@Column
 	private Long thumbNail; // 대표이미지 id
 	@Column
-	@ColumnDefault("판매중")
-	private String state; // 판매중, 판매완료
+	@ColumnDefault("true")
+	private String state; // 판매중(true), 판매완료(false)
 
 	public void removeStock(Integer productStock) throws Exception {
 		Integer restStock = this.productStock - productStock;
